c443aaaddf5597336b70d8a5fe562e84056b6285
DavMunHer <davidmunyozh@gmail.com>
2025-10-12 13:04:55 +0200

    feat: listing processes in an output file using processes

--- UsageInJava/src/main/java/org/example/Processes/T1S2/T2S1P2TasklistGuardaMunoz.java
+++ UsageInJava/src/main/java/org/example/Processes/T1S2/T2S1P2TasklistGuardaMunoz.java
@@ -1,4 +1,58 @@
 package org.example.Processes.T1S2;
 
+
+import java.io.*;
+
+/*
+Write a program that generates a process that saves the information about the processes
+in execution in a file named processes.txt.
+ */
 public class T2S1P2TasklistGuardaMunoz {
+    public static void main(String[] args) {
+        String os = System.getProperty("os.name").toLowerCase();
+        String command = "";
+
+        if (os.contains("win")) {
+            command = "C:\\>tasklist /v";
+        } else if (os.contains("nux") || os.contains("nix")) {
+            command = "ps -ef";
+        }
+        Runtime r = Runtime.getRuntime();
+        Process p = null;
+        BufferedReader br = null;
+        FileOutputStream fos = null;
+        PrintWriter pw = null;
+
+        try {
+            p = r.exec(command);
+            InputStream is = p.getInputStream();
+            br = new BufferedReader(new InputStreamReader(is));
+            fos = new FileOutputStream("processos.txt");
+            pw = new PrintWriter(fos);
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                pw.println(line);
+            }
+            pw.close();
+            fos.close();
+            br.close();
+            is.close();
+
+            int exitCode;
+
+            try {
+                exitCode = p.waitFor();
+                System.out.println("Exit code: " + exitCode);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
